ff5c043985cecd6ae943b14e7fd179afddc39084
unknown <rhysheal+moj@gmail.com>
2024-12-16 08:55:52 +0000

    formatted test classes

--- cypress/e2e/login_data_validation.cy.js
+++ cypress/e2e/login_data_validation.cy.js
@@ -1,81 +1,81 @@
 describe('Login Data Validation Tests for Ultimate QA Login Validation Demo', () => {
 
-    it('Username and password is valid', () => { 
-      cy.get('input[name="user[email]"]').should('exist');       
-      cy.get('input[name="user[email]"]').type("email@email.com")
+  it('Username and password is valid', () => {
+    cy.get('input[name="user[email]"]').should('exist');
+    cy.get('input[name="user[email]"]').type("email@email.com")
 
-      cy.get('input[name="user[password]"]').should('exist');
-      cy.get('input[name="user[password]"]').type(Cypress.env('user_password'))
+    cy.get('input[name="user[password]"]').should('exist');
+    cy.get('input[name="user[password]"]').type(Cypress.env('user_password'))
 
-      cy.contains('Sign in').should('be.visible');
-      cy.contains('Sign in').click()
-        
-        cy.get('.student-dashboard__welcome')
-        .invoke('text')        
-        .then((text) => {
-        const cleanedText = text.replace(/(\r\n|\n|\r)/g, '').trim();  
+    cy.contains('Sign in').should('be.visible');
+    cy.contains('Sign in').click()
+
+    cy.get('.student-dashboard__welcome')
+      .invoke('text')
+      .then((text) => {
+        const cleanedText = text.replace(/(\r\n|\n|\r)/g, '').trim();
         expect(cleanedText).to.equal('Welcome back, Auto T!');
-        });
-        
-        //checks user page is reached
-        //checks text links
-        cy.contains('My courses')
-        cy.contains('View more courses')
-        cy.contains('See Overview')
-        cy.contains('Start Course')
+      });
 
-        //checks elements
-        //course card
-        cy.get('.card__body--large').should('exist');   
-        
-        //user drop down
-        cy.get('.dropdown__toggle-button')
-        .invoke('text')               
-        .then((text) => {
-        
-        const cleanedText = text.replace(/(\r\n|\n|\r)/g, '')    
-        .replace(/\s+/g, '')           
-        .trim();                       
+    //checks user page is reached
+    //checks text links
+    cy.contains('My courses')
+    cy.contains('View more courses')
+    cy.contains('See Overview')
+    cy.contains('Start Course')
 
-        expect(cleanedText).to.equal('AutoT');                   
-        });
+    //checks elements
+    //course card
+    cy.get('.card__body--large').should('exist');
 
-        //my dashboard element
-        cy.get('.header__nav-item--default').should('exist');   
-        //search bar is available can be populated
-        cy.get('input[name="q"]').type("data can be input")
-        cy.get('input[name="q"]').should('exist');   
-      })
-    
-    it('Username field empty and password is valid', () => {
-        cy.get('input[name="user[password]"]').type('user_password')
-        cy.contains('Sign in').click()
-        cy.contains('Invalid email or password').should('be.visible');
-      })
+    //user drop down
+    cy.get('.dropdown__toggle-button')
+      .invoke('text')
+      .then((text) => {
 
-    it('Username field empty and password is empty', () => {
-        cy.contains('Sign in').click()
-        cy.contains('Invalid email or password').should('be.visible');
-      })
+        const cleanedText = text.replace(/(\r\n|\n|\r)/g, '')
+          .replace(/\s+/g, '')
+          .trim();
 
-    it('Username field populated with invalid email address (no @ symbol) and password is valid', () => {
-        cy.get('input[name="user[email]"]').type("emailemail.com")
-        cy.get('input[name="user[password]"]').type('user_password')
-        cy.contains('Sign in').click()
-        cy.contains('Invalid email or password').should('be.visible');
-      })
+        expect(cleanedText).to.equal('AutoT');
+      });
 
-    it('Username field populated with invalid email address (comma instead of a full stop) and password is valid', () => {
-        cy.get('input[name="user[email]"]').type("email@email,com")
-        cy.get('input[name="user[password]"]').type('user_password')
-        cy.contains('Sign in').click()
-        cy.contains('Invalid email or password').should('be.visible');
-      })
+    //my dashboard element
+    cy.get('.header__nav-item--default').should('exist');
+    //search bar is available can be populated
+    cy.get('input[name="q"]').type("data can be input")
+    cy.get('input[name="q"]').should('exist');
+  })
 
-    it('Username field populated with invalid email address (no full stop) and password is valid', () => {
-        cy.get('input[name="user[email]"]').type("email@emailcom")
-        cy.get('input[name="user[password]"]').type('user_password')
-        cy.contains('Sign in').click()
-        cy.contains('Invalid email or password').should('be.visible');
-      })
+  it('Username field empty and password is valid', () => {
+    cy.get('input[name="user[password]"]').type('user_password')
+    cy.contains('Sign in').click()
+    cy.contains('Invalid email or password').should('be.visible');
+  })
+
+  it('Username field empty and password is empty', () => {
+    cy.contains('Sign in').click()
+    cy.contains('Invalid email or password').should('be.visible');
+  })
+
+  it('Username field populated with invalid email address (no @ symbol) and password is valid', () => {
+    cy.get('input[name="user[email]"]').type("emailemail.com")
+    cy.get('input[name="user[password]"]').type('user_password')
+    cy.contains('Sign in').click()
+    cy.contains('Invalid email or password').should('be.visible');
+  })
+
+  it('Username field populated with invalid email address (comma instead of a full stop) and password is valid', () => {
+    cy.get('input[name="user[email]"]').type("email@email,com")
+    cy.get('input[name="user[password]"]').type('user_password')
+    cy.contains('Sign in').click()
+    cy.contains('Invalid email or password').should('be.visible');
+  })
+
+  it('Username field populated with invalid email address (no full stop) and password is valid', () => {
+    cy.get('input[name="user[email]"]').type("email@emailcom")
+    cy.get('input[name="user[password]"]').type('user_password')
+    cy.contains('Sign in').click()
+    cy.contains('Invalid email or password').should('be.visible');
+  })
 })
--- cypress/e2e/ui_login.cy.js
+++ cypress/e2e/ui_login.cy.js
@@ -1,47 +1,47 @@
 describe('UI Confirmation Tests for Ultimate QA Login Validation Demo', () => {
 
-    it('Confirm username field is present and information can be entered', () => {
-      cy.get('input[name="user[email]"]').type("data can be input")
-    })
-
-    it('Confirm password field is present and information can be entered', () => {
-      cy.get('input[name="user[password]"]').type("data can be input")
-    })
-
-    it('Confirm Remember Me radio checkbox is present can be clicked', () => {
-      cy.contains('Remember me').should('be.visible')
-      cy.contains('Remember me').click()
-    })
-
-    it('Confirm Forgot Password button is present and can be clicked', () => {
-      cy.contains('Forgot Password?').should('be.visible')
-      cy.contains('Forgot Password?').click()
-      //when clicked
-      cy.contains('Forgot your Password?').should('be.visible')
-    })
-
-    it('Confirm Sign in button is present at bottom of form', () => {
-      cy.contains('Sign in').click()
-
-      cy.contains('Invalid email or password').should('be.visible')
-    })
-
-    it('Confirm Sign In button is present at top of form', () => {
-      cy.contains('Sign In').should('be.visible')
-      cy.contains('Sign In').click()
-
-      cy.url().should('include', 'https://courses.ultimateqa.com/users/sign_in')
-    })
-
-    it('Confirm Create a new account button is present at top of form', () => {
-      cy.contains('Create a new account').should('be.visible')
-      cy.contains('Invalid email or password').should('not.exist')
-    })
-
-    it('Confirm vendor list is present', () => {
-      cy.contains('Or sign in with')
-      cy.get('.toga-icon-social-linkedIn').should('exist');   
-      cy.get('.toga-icon-social-facebook').should('exist');   
-      cy.get('.toga-icon-social-google-colorized').should('exist');
-    })
+  it('Confirm username field is present and information can be entered', () => {
+    cy.get('input[name="user[email]"]').type("data can be input")
   })
+
+  it('Confirm password field is present and information can be entered', () => {
+    cy.get('input[name="user[password]"]').type("data can be input")
+  })
+
+  it('Confirm Remember Me radio checkbox is present can be clicked', () => {
+    cy.contains('Remember me').should('be.visible')
+    cy.contains('Remember me').click()
+  })
+
+  it('Confirm Forgot Password button is present and can be clicked', () => {
+    cy.contains('Forgot Password?').should('be.visible')
+    cy.contains('Forgot Password?').click()
+    //when clicked
+    cy.contains('Forgot your Password?').should('be.visible')
+  })
+
+  it('Confirm Sign in button is present at bottom of form', () => {
+    cy.contains('Sign in').click()
+
+    cy.contains('Invalid email or password').should('be.visible')
+  })
+
+  it('Confirm Sign In button is present at top of form', () => {
+    cy.contains('Sign In').should('be.visible')
+    cy.contains('Sign In').click()
+
+    cy.url().should('include', 'https://courses.ultimateqa.com/users/sign_in')
+  })
+
+  it('Confirm Create a new account button is present at top of form', () => {
+    cy.contains('Create a new account').should('be.visible')
+    cy.contains('Invalid email or password').should('not.exist')
+  })
+
+  it('Confirm vendor list is present', () => {
+    cy.contains('Or sign in with')
+    cy.get('.toga-icon-social-linkedIn').should('exist');
+    cy.get('.toga-icon-social-facebook').should('exist');
+    cy.get('.toga-icon-social-google-colorized').should('exist');
+  })
+})
